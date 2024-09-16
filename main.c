a95b1949ea01a1970d14594826bfd8c12f89b810
S12ou <malakaliassam23@gmail.com>
2024-09-16 14:20:30 +0100

    Changed message to 'Hello School' and updated the loop count to 32

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
-for (int i = 0; i < 32; i++) {
-    printf("Hello School\n");
+#include <stdio.h>
+int main() {
+    for (int i = 0; i < 32; i++) {
+        printf("Hello School\n");
+    }
+    return 0;
 }
